a73bab8074a904119e2b50346b4f57d647be0e25
Jake Kreider <jake@jakekreider.com>
2013-11-23 13:39:11 -0600

    Cleanup

---
+++ compass/compass_test.go
@@ -0,0 +1,23 @@
+package compass
+
+import (
+	"testing"
+)
+
+func TestGetCompassPoints(t *testing.T) {
+	var values = map[float32]string{
+		0:   "N",
+		90:  "E",
+		180: "S",
+		270: "W",
+		225: "SW",
+		359: "N",
+	}
+	for value, expected := range values {
+		result := GetCompassAbbreviation(value)
+		if result != expected {
+			t.Errorf("Incorrect value for %v, expected %s but got %s", value, expected, result)
+		}
+	}
+
+}
